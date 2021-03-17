370f859c841a9059910f159706eef53d3c5bf7e4
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-17 11:16:16 -0700

    feat: `getWebFlowAuthorizationUrl()`

--- src/get-web-flow-authorization-url.ts
+++ src/get-web-flow-authorization-url.ts
@@ -1,1 +1,52 @@
-export function getWebFlowAuthorizationUrl() {}
+import {
+  oauthAuthorizationUrl,
+  OAuthAppResult,
+  GitHubAppResult,
+} from "@octokit/oauth-authorization-url";
+import { request as defaultRequest } from "@octokit/request";
+import { RequestInterface } from "@octokit/types";
+
+import { requestToOAuthBaseUrl } from "./utils";
+
+type OAuthAppOptions = {
+  clientId: string;
+
+  clientType?: "oauth-app";
+  allowSignup?: boolean;
+  login?: string;
+  scopes?: string | string[];
+  redirectUrl?: string;
+  state?: string;
+  request?: RequestInterface;
+};
+
+type GitHubAppOptions = {
+  clientId: string;
+
+  clientType: "github-app";
+  allowSignup?: boolean;
+  login?: string;
+  redirectUrl?: string;
+  state?: string;
+  request?: RequestInterface;
+};
+
+export function getWebFlowAuthorizationUrl(
+  options: OAuthAppOptions
+): OAuthAppResult;
+export function getWebFlowAuthorizationUrl(
+  options: GitHubAppOptions
+): GitHubAppResult;
+
+export function getWebFlowAuthorizationUrl({
+  request = defaultRequest,
+  ...options
+}: OAuthAppOptions | GitHubAppOptions): OAuthAppResult | GitHubAppResult {
+  const baseUrl = requestToOAuthBaseUrl(request);
+
+  // @ts-expect-error TypeScript wants `clientType` to be set explicitly ¯\_(ツ)_/¯
+  return oauthAuthorizationUrl({
+    ...options,
+    baseUrl,
+  });
+}
