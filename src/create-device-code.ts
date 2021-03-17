dd307f60a880e125ecda3bcc892667ba667f4529
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-17 15:40:55 -0700

    feat: `createDeviceCode()`

--- src/create-device-code.ts
+++ src/create-device-code.ts
@@ -1,1 +1,47 @@
-export async function createDeviceCode() {}
+import { request as defaultRequest } from "@octokit/request";
+import { OctokitResponse, RequestInterface } from "@octokit/types";
+
+import { requestToOAuthBaseUrl } from "./utils";
+
+type OAuthAppOptions = {
+  clientType: "oauth-app";
+  clientId: string;
+  scopes?: string[];
+  request?: RequestInterface;
+};
+type GitHubAppOptions = {
+  clientType: "github-app";
+  clientId: string;
+  request?: RequestInterface;
+};
+
+type DeviceTokenResponse = {
+  device_code: string;
+  user_code: string;
+  verification_uri: string;
+  expires_in: number;
+  interval: number;
+};
+
+export async function createDeviceCode(
+  options: OAuthAppOptions | GitHubAppOptions
+): Promise<OctokitResponse<DeviceTokenResponse>> {
+  const request =
+    options.request ||
+    /* istanbul ignore next: we always pass a custom request in tests */
+    defaultRequest;
+
+  const parameters: Record<string, unknown> = {
+    baseUrl: requestToOAuthBaseUrl(request),
+    headers: {
+      accept: "application/json",
+    },
+    client_id: options.clientId,
+  };
+
+  if ("scopes" in options && Array.isArray(options.scopes)) {
+    parameters.scope = options.scopes.join(" ");
+  }
+
+  return request("POST /login/device/code", parameters);
+}
