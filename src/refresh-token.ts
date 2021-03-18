935aed06140c3ac0995532619e8e03676385ca6d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-17 17:50:28 -0700

    feat: `refreshToken()`

--- src/refresh-token.ts
+++ src/refresh-token.ts
@@ -1,1 +1,62 @@
-export async function refreshToken() {}
+import { request as defaultRequest } from "@octokit/request";
+import { OctokitResponse, RequestInterface } from "@octokit/types";
+
+import {
+  GitHubAppAuthenticationWithExpiration,
+  GitHubAppCreateTokenWithExpirationResponseData,
+} from "./types";
+import { oauthRequest } from "./utils";
+
+type Options = {
+  clientType: "github-app";
+  clientId: string;
+  clientSecret: string;
+  refreshToken: string;
+  request?: RequestInterface;
+};
+
+export async function refreshToken(
+  options: Options
+): Promise<
+  OctokitResponse<GitHubAppCreateTokenWithExpirationResponseData> & {
+    authentication: GitHubAppAuthenticationWithExpiration;
+  }
+> {
+  const request =
+    options.request ||
+    /* istanbul ignore next: we always pass a custom request in tests */
+    defaultRequest;
+
+  const response = await oauthRequest(
+    request,
+    "POST /login/oauth/access_token",
+    {
+      client_id: options.clientId,
+      client_secret: options.clientSecret,
+      grant_type: "refresh_token",
+      refresh_token: options.refreshToken,
+    }
+  );
+
+  const apiTimeInMs = new Date(response.headers.date as string).getTime();
+  const authentication: GitHubAppAuthenticationWithExpiration = {
+    clientType: "github-app",
+    clientId: options.clientId,
+    clientSecret: options.clientSecret,
+    token: response.data.access_token,
+    refreshToken: response.data.refresh_token,
+    expiresAt: toTimestamp(apiTimeInMs, response.data.expires_in),
+    refreshTokenExpiresAt: toTimestamp(
+      apiTimeInMs,
+      response.data.refresh_token_expires_in
+    ),
+  };
+
+  delete response.data.scope;
+
+  return { ...response, authentication };
+}
+
+function toTimestamp(apiTimeInMs: number, expirationInSeconds: number) {
+  return new Date(apiTimeInMs + expirationInSeconds * 1000).toISOString();
+}
