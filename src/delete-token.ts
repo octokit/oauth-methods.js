0dd6ae5cae86518924c1a6ab2787d33abcfcf3a7
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-17 21:59:31 -0700

    feat: `deleteToken()`

--- src/delete-token.ts
+++ src/delete-token.ts
@@ -1,1 +1,45 @@
-export async function deleteToken() {}
+import { request as defaultRequest } from "@octokit/request";
+import { RequestInterface, Endpoints } from "@octokit/types";
+import btoa from "btoa-lite";
+
+type OAuthAppOptions = {
+  clientType: "oauth-app";
+  clientId: string;
+  clientSecret: string;
+  token: string;
+  request?: RequestInterface;
+};
+type GitHubAppOptions = {
+  clientType: "github-app";
+  clientId: string;
+  clientSecret: string;
+  token: string;
+  request?: RequestInterface;
+};
+
+export async function deleteToken(
+  options: OAuthAppOptions
+): Promise<Endpoints["DELETE /applications/{client_id}/token"]["response"]>;
+export async function deleteToken(
+  options: GitHubAppOptions
+): Promise<Endpoints["DELETE /applications/{client_id}/token"]["response"]>;
+
+export async function deleteToken(
+  options: OAuthAppOptions | GitHubAppOptions
+): Promise<any> {
+  const request =
+    options.request ||
+    /* istanbul ignore next: we always pass a custom request in tests */
+    defaultRequest;
+
+  const auth = btoa(`${options.clientId}:${options.clientSecret}`);
+  const response = await request("DELETE /applications/{client_id}/token", {
+    headers: {
+      authorization: `basic ${auth}`,
+    },
+    client_id: options.clientId,
+    access_token: options.token,
+  });
+
+  return response;
+}
